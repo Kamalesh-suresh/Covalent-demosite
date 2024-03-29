2d1d7f11bdd3f3df7f4bd6dd6bc634e395e7417f
kamalesh.suresh <kamalesh.suresh@psiog.com>
2024-03-29 08:29:36 +0530

    Added Aceternity UI

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,19 +1,30 @@
-/*
- * Copyright 2022 Agnostiq Inc.
- * Note: This file is subject to a proprietary license agreement entered into between
- * you (or the person or organization that you represent) and Agnostiq Inc. Your rights to
- * access and use this file is subject to the terms and conditions of such agreement.
- * Please ensure you carefully review such agreements and, if you have any questions
- * please reach out to Agnostiq at: [support@agnostiq.com].
- */
-
 import React from "react";
+import { cn } from "@/utils/cn";
+import { Spotlight } from "../components/ui/spotlight";
 
 function page() {
   return (
-    <h1 className="font-quicksand text-3xl font-bold underline">
-      This is contacts page
-    </h1>
+    // <h1 className="font-quicksand text-3xl font-bold underline sm:bg-blue-medium md:bg-blue-light xl:bg-blue-dark 2xl:bg-white">
+    //   This is contacts page
+    // </h1>
+    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Welcome <br />
+          to contacts page.
+        </h1>
+        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+          Spotlight effect is a great way to draw attention to a specific part
+          of the page. Here, we are drawing the attention towards the text
+          section of the page. I don&apos;t know why but I&apos;m running out of
+          copy.
+        </p>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,8 @@
+// prettier-ignore
 import type { Config } from "tailwindcss";
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
   content: [
@@ -6,12 +10,20 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     colors: {
       white: "#EEF5FF",
       "blue-light": "#B4D4FF",
       "blue-medium": "#86B6F6",
       "blue-dark": "#176B87",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     extend: {
       fontFamily: {
@@ -22,8 +34,35 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
 export default config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
